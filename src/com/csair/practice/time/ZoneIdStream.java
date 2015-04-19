939cfdfe185bdf3bcdbe1cfa069ac4f2021f33c7
bbfking <bbfking@gmail.com>
2015-04-19 23:46:34 +0800

    zoneid with stream

---
+++ src/com/csair/practice/time/ZoneIdStream.java
@@ -0,0 +1,17 @@
+package com.csair.practice.time;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.stream.Stream;
+
+public class ZoneIdStream {
+	public static void main(String[] args) {
+		LocalDateTime ldt = LocalDateTime.now();
+		Stream <String> zoneIdStream = ZoneId.getAvailableZoneIds().stream();
+		zoneIdStream.forEach((x)->{
+				ZonedDateTime zdt = ldt.atZone(ZoneId.of(x));
+				System.out.println(x+" : " + zdt.getOffset());
+			});
+	}
+}
